ad7b2cb5ca6881e492cad215fdb01189e84d7155
b13263913 <batuhanmutlu@std.iyte.edu.tr>
2020-10-19 09:16:40 +0000

    .

--- lab1/example4.py
+++ lab1/example4.py
@@ -1,6 +1,6 @@
 a = 2
 b = 6
 c = -20
-root_1 = (-b-(b**2-4*a*c)**0.5)/2*a
-root_2 = (-b+(b**2-4*a*c)**0.5)/2*a
+root_1 = (-b-(b**2-4*a*c)**0.5)/(2*a)
+root_2 = (-b+(b**2-4*a*c)**0.5)/(2*a)
 print ("The roots of this equation is:", root_1,"and",root_2,".")
