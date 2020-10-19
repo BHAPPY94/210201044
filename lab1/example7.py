936caf46781a5f28c10984bb6312157a65332b65
b13263913 <batuhanmutlu@std.iyte.edu.tr>
2020-10-19 09:56:03 +0000

    .

--- lab1/example7.py
+++ lab1/example7.py
@@ -4,4 +4,4 @@ distance = 490
 last_distance = 150
 minutes = 60
 time_elapsed = (distance-last_distance)/(car1+car2)*(minutes)
-print (time_elapsed,"minutes later distance between cars will be 150km")
+print (time_elapsed,"minutes later distance between cars will be 150km.")
