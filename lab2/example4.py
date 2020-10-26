b27c069abb7166974aee09b2fdfd79bd2f25eccc
b13263913 <batuhanmutlu@std.iyte.edu.tr>
2020-10-26 09:13:20 +0000

    .

--- lab2/example4.py
+++ lab2/example4.py
@@ -4,7 +4,7 @@ if age <6 or age >60:
   ticket_price = 0
   print ("Free! Your payment is:", ticket_price)
 elif age>=6 and age<18:
-  ticket_price = 3 * 0.5
+  ticket_price *= 0.5
   print ("Your payment is:", ticket_price)
 elif age >=18 and age <= 60:
   print ("Your payment is:", ticket_price)
