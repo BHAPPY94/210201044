c240489ba26dc28c6f7fd3c1b553535618d6ae5d
b13263913 <batuhanmutlu@std.iyte.edu.tr>
2020-12-14 08:41:03 +0000

    .

---
+++ lab7/example1.py
@@ -0,0 +1,11 @@
+
+
+def sum_of_list(a_list):
+  summ = 0
+  for i in a_list:
+    summ += i
+  return summ
+
+a_list = [12, -7, 5, -89.4, 3, 27, 56, 57.3]
+summ = sum_of_list(a_list)
+print(summ**2)
